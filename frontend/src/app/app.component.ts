5d7d40e9fe550a64b84cc7ae4324af99e6031287
Kurnat Vasil <beda21@ukr.net>
2021-08-15 21:03:56 +0300

    deleted service www.omdbapi.com

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.http
-      .get('http://www.omdbapi.com/?s=one&apikey=c39de0df')
-      .subscribe((data) => console.log(data));
-  }
-}
+export class AppComponent {}
